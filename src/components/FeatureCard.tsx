ca881faef2d01cf0e415ee08e7b3c1f303dff396
Ritesh Singh <ritesh@tiobu.com>
2023-06-30 12:20:44 +0530

    feature card fix

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -4,18 +4,35 @@ type Payload = {
 
 const FeatureCard = ({ margin }: Payload) => {
   return (
-    <div
-      style={{ border: '1px solid #7A3199' }}
-      className={`col-span-1 p-5 shadow-md w-[280px] h-[160px] mt-${margin} rounded-xl `}
-    >
-      <h1 className="font-bold text-2xl pb-4 text-black ">
-        Interactive Features
-      </h1>
-      <p className="text-black font-normal text-sm ">
-        Lorem Ipsum is simply dummy text of the printing and typesetting in
-        ustry. Lorem Ipsum has been
-      </p>
-    </div>
+    <>
+      {margin === '5' ? (
+        <div
+          style={{ border: '1px solid #7A3199' }}
+          className={`col-span-1 p-5 shadow-md w-[280px] h-[160px] mt-5 rounded-xl `}
+        >
+          <h1 className="font-bold text-2xl pb-4 text-black ">
+            Interactive Features
+          </h1>
+          <p className="text-black font-normal text-sm ">
+            Lorem Ipsum is simply dummy text of the printing and typesetting in
+            ustry. Lorem Ipsum has been
+          </p>
+        </div>
+      ) : (
+        <div
+          style={{ border: '1px solid #7A3199' }}
+          className={`col-span-1 p-5 shadow-md w-[280px] h-[160px] rounded-xl `}
+        >
+          <h1 className="font-bold text-2xl pb-4 text-black ">
+            Interactive Features
+          </h1>
+          <p className="text-black font-normal text-sm ">
+            Lorem Ipsum is simply dummy text of the printing and typesetting in
+            ustry. Lorem Ipsum has been
+          </p>
+        </div>
+      )}
+    </>
   )
 }
 
